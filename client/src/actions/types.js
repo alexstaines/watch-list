b0c0c550e46efec307c6fda9dda1e9c97e17b4f0
alexstaines <70612633+alexstaines@users.noreply.github.co>
2022-08-10 00:52:01 +1000

    added initial TYPES for reducer actions

---
+++ client/src/actions/types.js
@@ -0,0 +1,16 @@
+export const SET_ALERT = 'SET_ALERT';
+export const REMOVE_ALERT = 'REMOVE_ALERT';
+export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
+export const REGISTER_FAIL = 'REGISTER_FAIL';
+export const USER_LOADED = 'USER_LOADED';
+export const AUTH_ERROR = 'AUTH_ERROR';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAIL = 'LOGIN_FAIL';
+export const LOGOUT = 'LOGOUT';
+
+export const GET_LIST = 'GET_LIST';
+
+export const ADD_LIST_ITEM = 'ADD_LIST_ITEM';
+export const REMOVE_LIST_ITEM = 'REMOVE_LIST_ITEM';
+export const UPDATE_LIST_ITEM = 'UPDATE_LIST_ITEM';
+export const LIST_ERROR = 'LIST_ERROR';
