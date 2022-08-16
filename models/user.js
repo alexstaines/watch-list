0bd60d50174d0d03445454b2c895b6fbbdd49355
alexstaines <70612633+alexstaines@users.noreply.github.com>
2022-08-16 18:50:15 +1000

    added 'listTitle' attribute for text to show at top of personal listing. TODO: backend request for this to be customised

--- models/user.js
+++ models/user.js
@@ -18,6 +18,9 @@ const UserSchema = new mongoose.Schema({
     type: Boolean,
     default: false,
   },
+  listTitle: {
+    type: String,
+  },
 });
 
 module.exports = User = mongoose.model("user", UserSchema);
