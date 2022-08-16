8b506d65ba2a623b4d5a38dcaa6c03a48fa55704
alexstaines <70612633+alexstaines@users.noreply.github.com>
2022-08-16 18:44:14 +1000

    updated with latest components (Landing, Login, Register, List)

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,43 @@
-import React, { Fragment } from "react";
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+import List from "./components/layout/List";
+import Alert from "./components/layout/Alert";
+import PrivateRoute from "./components/routing/PrivateRoute";
+// Redux
+import { Provider } from "react-redux";
+import store from "./store";
+import { loadUser } from "./actions/auth";
+import setAuthToken from "./utils/setAuthToken";
+
 import "./App.css";
 
-const App = () => (
-  <Fragment>
-    <div className="App">App</div>
-  </Fragment>
-);
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
+  return (
+    <Provider store={store}>
+      <Router>
+        <Navbar />
+        <Alert />
+        <Routes>
+          <Route exact path="/" element={<Landing />} />
+          <Route exact path="login" element={<Login />} />
+          <Route exact path="register" element={<Register />} />
+          <Route exact path="list" element={<PrivateRoute component={List} />} />
+        </Routes>
+      </Router>
+    </Provider>
+  );
+};
 
 export default App;
