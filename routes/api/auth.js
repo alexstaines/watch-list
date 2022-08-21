52ac78f3e80f5e14a8830a8771880c2e73dea5a7
alexstaines <70612633+alexstaines@users.noreply.github.com>
2022-08-21 23:40:10 +1000

    added toggle-visibility post functionality

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 const auth = require("../../middleware/auth");
 const jwt = require("jsonwebtoken");
 const config = require("config");
@@ -22,55 +22,74 @@ router.get("/", auth, async (req, res) => {
   }
 });
 
-
 // @route POST API/Auth
 // @desc Authenticate user & get token
 // @access Public
 router.post(
-    "/",
-    [
-      check("username", "Username is required.").not().isEmpty(),
-      check("password", "Please enter password.").not().isEmpty(),
-    ],
-    async (req, res) => {
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-      }
-  
-      console.log(req.body);
-      const { username, password } = req.body;
-  
-      try {
-        // Check if user exists
-        let user = await User.findOne({ username });
-  
-        if (!user) {
-          return res.status(400).json({ errors: [{ msg: "Invalid Credentials" }] });
-        }
-
-        const isMatch = await bcrypt.compare(password, user.password);
-
-        if (!isMatch) {
-            return res.status(400).json({ errors: [{ msg: "Invalid Credentials" }] });
-        }
-  
-        // Return jsonwebtoken
-        const payload = {
-          user: {
-            id: user.id,
-          },
-        };
-        jwt.sign(payload, config.get("jwtToken"), { expiresIn: 360000 }, (err, token) => {
-          if (err) throw err;
-          res.json({ token });
-          console.log(token);
-        });
-      } catch (err) {
-        console.error(err.message);
-        res.status(500).send("Server error");
-      }
+  "/",
+  [
+    check("username", "Username is required.").not().isEmpty(),
+    check("password", "Please enter password.").not().isEmpty(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-  );
+
+    console.log(req.body);
+    const { username, password } = req.body;
+
+    try {
+      // Check if user exists
+      let user = await User.findOne({ username });
+
+      if (!user) {
+        return res.status(400).json({ errors: [{ msg: "Invalid Credentials" }] });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res.status(400).json({ errors: [{ msg: "Invalid Credentials" }] });
+      }
+
+      // Return jsonwebtoken
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+      jwt.sign(payload, config.get("jwtToken"), { expiresIn: 360000 }, (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+        console.log(token);
+      });
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
+  }
+);
+
+// @route POST API/Auth/toggle-visibility
+// @desc Toggle visibility
+// @access Private
+router.post("/toggle-visibility", auth, async (req, res) => {
+  try {
+    const user = await User.findById({ _id: req.user.id });
+
+    const updated_user = await User.findByIdAndUpdate(
+      { _id: req.user.id },
+      { visibility: !user.visibility },
+      { new: true }
+    );
+
+    res.json(updated_user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
 
 module.exports = router;
