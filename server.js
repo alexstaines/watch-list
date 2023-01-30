fb8dd92b17a6ea882a9924a0c00b94134ccf11a8
alexstaines <70612633+alexstaines@users.noreply.github.com>
2023-01-30 16:41:07 +1100

    .

--- server.js
+++ server.js
@@ -18,38 +18,3 @@ app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/list", require("./routes/api/list"));
 
 app.listen(port, () => console.log("Server is running on Port: " + port));
-
-// router.get("/", function (req, res) {
-//   res.sendFile(path.join(__dirname + "/index.html"));
-//   //__dirname : It will resolve to your project folder.
-// });
-
-//app.use(express.static("public"));
-
-//app.use("/", router);
-
-// app.get("/express_backend", (req, res) => {
-//   res.send({ express: "Express Backend Connected to React" });
-// });
-
-// app.listen(port, () => console.log("Server is running on Port: " + port));
-
-// var data = [
-//   {
-//     title: "Made in Abyss",
-//     averageRating: 8.5,
-//     synopsis: "lovcraftian ghibli",
-//     watched: false,
-//     watchedEps: 15,
-//   },
-// ];
-
-// router.route("/insertdata").post(function (req, res) {
-//     anime.insertMany(data, function (err, result) {
-//       if (err) {
-//         res.send(err);
-//       } else {
-//         res.send(result);
-//       }
-//     });
-//   });
